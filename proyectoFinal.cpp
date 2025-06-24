065f1bddfb83d73ce8e0d51aee8d48d862e336e2
donabdel02 <abdeonzo9812@gmail.com>
2025-06-24 16:19:51 -0600

    segundo push

--- proyectoFinal.cpp
+++ proyectoFinal.cpp
@@ -21,3 +21,41 @@ struct Jugador {
     int puntaje;
     char nombre[50];
 };
+void inicializarMazo(Carta* mazo, int& tam) {
+    const char* nombres[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    int valores[] = {2,3,4,5,6,7,8,9,10,10,10,10,11};
+    tam = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 13; j++) {
+            strcpy(mazo[tam].nombre, nombres[j]);
+            mazo[tam].valor = valores[j];
+            tam++;
+        }
+    }
+    for (int i = 0; i < tam; i++) {
+        int r = rand() % tam;
+        swap(mazo[i], mazo[r]);
+    }
+}
+
+void mostrarMano(Jugador* jugador, bool ocultarPrimera = false) {
+    cout << "Cartas: ";
+    for (int i = 0; i < jugador->cantidad; i++) {
+        if (ocultarPrimera && i == 0) cout << "[??] ";
+        else cout << "[" << jugador->mano[i].nombre << "] ";
+    }
+    cout << endl;
+}
+
+int calcularPuntaje(Jugador* jugador) {
+    int total = 0, ases = 0;
+    for (int i = 0; i < jugador->cantidad; i++) {
+        total += jugador->mano[i].valor;
+        if (strcmp(jugador->mano[i].nombre, "A") == 0) ases++;
+    }
+    while (total > 21 && ases > 0) {
+        total -= 10;
+        ases--;
+    }
+    return total;
+}
